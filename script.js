1989c945b21e415cceacf88e9bac555912a4a3a6
Park JaeKwang <sowkdxkd12@gmail.com>
2024-12-03 15:34:55 +0900

    test

---
+++ script.js
@@ -0,0 +1,31 @@
+function calculate() {
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+
+  let option = document.getElementById("operation").value;
+  let result = "";
+
+  if (isNaN(num1) || isNaN(num2)) result = "Input Error!!";
+  else {
+    switch (option) {
+      case "add":
+        result = num1 + num2;
+        break;
+      case "subtract":
+        result = num1 - num2;
+        break;
+      case "multiply":
+        result = num1 * num2;
+        break;
+      case "divide":
+        if (num2 == 0) result = "Error Divided By 0";
+        result = num1 % num2;
+        break;
+      default:
+        result = "Invalid operation!";
+        break;
+    }
+  }
+
+  document.getElementById("result").textContent = "Result: " + result;
+}
